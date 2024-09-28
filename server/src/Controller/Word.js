937999816bc71a177895c0f91a7492e91b4fc185
Ayan Sheikh <ayansheikh52@gmail.com>
2024-09-28 20:22:02 +0530

    added more word endpoints

--- server/src/Controller/Word.js
+++ server/src/Controller/Word.js
@@ -3,9 +3,37 @@ import { encrypt } from "../utils/encryption.js";
 
 export const addWord = async (req, res) => {
   try {
-    const { word } = req.body;
-    const newWord = await Word.create({ word });
+    const { word ,difficulty } = req.body;
+    const newWord = await Word.create({ word ,difficulty});
     res.status(200).json(newWord);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+export const getNumberOfWords = async (req, res) => {
+  try {
+    const count = await Word.countDocuments();
+    res.status(200).json({ count });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+export const addManyWords = async (req, res) => {
+  try {
+    const words = req.body;
+    const newWords = await Word.insertMany(words);
+    res.status(200).json(newWords);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+export const removeAllWords = async (req, res) => {
+  try {
+    await Word.deleteMany({});
+    res.status(200).json({ message: "All words removed successfully" });
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
@@ -20,10 +48,8 @@ export const getRandomWord = async (req, res) => {
   try {
     const word = await Word.aggregate([{ $sample: { size: 1 } }]);
     const encryptedWord = encrypt(word[0].word);
-    console.log(encryptedWord);
-    res.status(200).json({ word: encryptedWord, id: word[0]._id });
+    res.status(200).json({ word: encryptedWord, id: word[0]._id,difficulty:word[0].difficulty });
   } catch (err) {
-    console.log(err);
     res.status(400).json({ message: err.message });
   }
 };
--- server/src/Route/Word.js
+++ server/src/Route/Word.js
@@ -1,11 +1,15 @@
 import express from 'express'
-import { addWord, getRandomWord } from '../Controller/Word.js'
+import { addManyWords, addWord, getNumberOfWords, getRandomWord, removeAllWords } from '../Controller/Word.js'
 const router = express.Router()
 
 router.use(express.json())
 
 
 router.post("/",addWord)
+router.post("/many",addManyWords)
+router.get("/count",getNumberOfWords)
+
+router.delete("/",removeAllWords)
 router.get("/random",getRandomWord)
 
 export const wordRouter = router
