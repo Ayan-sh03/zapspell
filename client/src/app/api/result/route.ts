354b7cab2b591610b29ba2e33f67644b877c0896
Ayan Sheikh <ayansheikh52@gmail.com>
2024-09-30 22:47:31 +0530

    minor fixes

--- client/src/app/api/result/route.ts
+++ client/src/app/api/result/route.ts
@@ -13,5 +13,7 @@ export async function GET(req: Request) {
 
     const data = await res.json();
     return NextResponse.json({ data }, { status: 200 });
-  } catch (error) {}
+  } catch (error) {
+    return NextResponse.json({ error: "Something went wrong" }, { status: 500 });
+  }
 }
